402b02f60e100fc90d34f5718123d118021e8dd4
Matthias Kübrich <matthias.kuebrich@t-online.de>
2026-02-07 23:40:22 +0100

    Remove time-zone shifts when dividing, to support different midnight time stamps

--- src/plotfox/ticker.py
+++ src/plotfox/ticker.py
@@ -5,14 +5,14 @@ import pandas as pd
 class Ticker:
     def __init__(self, ticker: str):
         self._ticker = yf.Ticker(ticker)
-    
+
     def __str__(self) -> str:
         return self._ticker.ticker
 
     def history(self, period: None | str = None) -> pd.DataFrame:
         return self._ticker.history(period=period)
 
-    def __truediv__(self, other: "Ticker") -> "Ticker":
+    def __truediv__(self, other: "Ticker") -> "DivTicker":
         return DivTicker(self, other)
 
 
@@ -23,13 +23,16 @@ class DivTicker(Ticker):
     def __init__(self, dividend: Ticker, divisor: Ticker):
         self.dividend = dividend
         self.divisor = divisor
-    
+
     def __str__(self) -> str:
         return f"{self.dividend}/{self.divisor}"
 
     def history(self, *args, **kwargs) -> pd.DataFrame:
         dividend_df = self.dividend.history(*args, **kwargs)
         divisor_df = self.divisor.history(*args, **kwargs)
+
+        dividend_df.index = dividend_df.index.tz_localize(None)  # ty:ignore[possibly-missing-attribute]
+        divisor_df.index = divisor_df.index.tz_localize(None)  # ty:ignore[possibly-missing-attribute]
 
         df = pd.DataFrame(
             data={
@@ -39,4 +42,4 @@ class DivTicker(Ticker):
             index=dividend_df.index,
         )
 
-        return df
+        return df.dropna()
